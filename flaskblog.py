0f8254a8732713cb1c8e66b58351fee882f53124
Yawar mushtaq <yawarmushtaq52@gmai.com>
2020-06-12 00:34:18 +0530

    added session_ and consequently modifeid layout.htm

--- flaskblog.py
+++ flaskblog.py
@@ -8,7 +8,7 @@ db=sq.connect(
     database="flask"
 )
 mycur=db.cursor()
-
+session_={}
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='fwehdfaisuhfu'
@@ -42,19 +42,23 @@ def home(author):
         result1=mycur.fetchall()
         for k in result1:
             ListContent.append(k[0])
-    return render_template("home.htm",title="home",posts=ListContent) 
+    return render_template("home.htm",title="home",posts=ListContent,session=session_) 
 
-@app.route('/about',methods=['GET','POST'])
-def about():
+@app.route('/write',methods=['GET','POST'])
+def write():
+    if not(session_):
+        return redirect(url_for('login'))
     form=PostForm()
     if form.validate_on_submit():
         return redirect(url_for("login"))
     else:
-        return render_template('about.htm',
-                            title="about",form=form)
+        return render_template('write.htm',
+                            title="write",form=form,session=session_)
 
 @app.route("/register",methods=['GET','POST'])
 def register():
+    if session_:
+        return home(session_['user'])
     form=RegistrationForm()
     if form.validate_on_submit():
         mycur.execute(f"SELECT * FROM reg WHERE user='{form.username.data}' ")
@@ -73,26 +77,34 @@ def register():
         else:
             flash("Username taken,try a diffrent one",'danger')
         return render_template('register.htm',
-                            form=form,title='Register')
+                            form=form,title='Register',session=session_)
     else:
         return render_template('register.htm',
-                            form=form,title='Register')
+                            form=form,title='Register',session=session_)
 @app.route("/")
 @app.route('/login',methods=['POST','GET'])
 def login():
+    if session_:
+        return home(session_['user'])
     form=LoginForm()
     if form.validate_on_submit():
         mycur.execute(f"SELECT pass FROM reg WHERE user='{form.username.data}'")
         result=mycur.fetchone()
         if result:
             if form.password.data==result[0]:
+                session_['user']=form.username.data
                 flash('Login Successful','success')
                 return home(form.username.data)
             else:
-                flash('Login unsuccessful,please check your email and/or password','danger')
+                flash('Login unsuccessful,please check your password','danger')
         else:
              flash('Account does not exist','danger')            
-    return render_template('login.htm',title='Login',form=form)
+    return render_template('login.htm',title='Login',form=form,session=session_)
+
+@app.route('/logout')
+def logout():
+    session_.clear()
+    return redirect(url_for('login'))
      
 
 if __name__=='__main__':
